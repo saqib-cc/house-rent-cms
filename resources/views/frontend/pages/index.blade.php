175eca4ad6a8c7ed8b06eb2f391d0601dbce49de
saqib_rehman <saqibrehman139@gmail.com>
2022-11-30 13:06:28 +0500

    first commit

--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -473,30 +473,32 @@
               </div><!-- /.col-md-12 -->
           </div><!-- /.row -->
           <div class="row">
+              <?php $properties = App\Property::all(); ?>
+              @foreach ($properties as $property)
               <div class="col-md-4 col-sm-6 col-xs-6">
                   <div class="apartments-content">
                       <div class="image-content">
-                         <a href="apartment-single.html"><img src="{{asset('frontend/assets/images/apartment/apartment-one.png')}}" alt="apartment" /></a>
+                         <a href="apartment-single.html"><img src="{{$property->image}}" alt="apartment" style="height: 300px;width:300px;" /></a>
                       </div><!-- /.image-content -->
                  
                       <div class="text-content">
                           <div class="top-content">
-                             <h3><a href="apartment-single.html">Family Apartment</a></h3>
+                             <h3><a href="apartment-single.html">{{$property->title}}</a></h3>
                              <span>
                                  <i class="fa fa-map-marker"></i>
-                                 Dhanmondi, Dhaka
+                                 {{$property->address}}
                              </span> 
                           </div><!-- /.top-content -->
                           <div class="bottom-content clearfix">
                               <div class="meta-bed-room">
-                                 <i class="fa fa-bed"></i> 3 Bedrooms
+                                 <i class="fa fa-bed"></i> {{$property->bedroom}}
                               </div>
                               <div class="meta-bath-room">                                
-                                 <i class="fa fa-bath"></i>2 Bathroom
+                                 <i class="fa fa-bath"></i>{{$property->bathroom}}
                               </div>
                               <span class="clearfix"></span>
                               <div class="rent-price pull-left">
-                                 $200
+                                {{$property->price}}
                               </div>
                               <div class="share-meta dropup pull-right">
                                   <ul>
@@ -526,274 +528,7 @@
                       </div><!-- /.text-content -->
                   </div><!-- /.partments-content -->
               </div><!-- /.col-md-4 -->
-              <div class="col-md-4 col-sm-6 col-xs-6">
-                  <div class="apartments-content">
-                      <div class="image-content">
-                          <a href="apartment-single.html"><img src="{{asset('frontend/assets/images/apartment/apartment-two.png')}}" alt="apartment" /></a>
-                      </div><!-- /.image-content -->
-                  
-                      <div class="text-content">
-                          <div class="top-content">
-                              <h3><a href="apartment-single.html">Family Apartment</a></h3>
-                              <span>
-                                  <i class="fa fa-map-marker"></i>
-                                  Dhanmondi, Dhaka
-                              </span> 
-                          </div><!-- /.top-content -->
-                          <div class="bottom-content clearfix">
-                             <div class="meta-bed-room">
-                                 <i class="fa fa-bed"></i> 3 Bedrooms
-                             </div>
-                             <div class="meta-bath-room">                                
-                                 <i class="fa fa-bath"></i>2 Bathroom
-                             </div>
-                             <span class="clearfix"></span>
-                             <div class="rent-price pull-left">
-                                 $200
-                             </div>
-                             <div class="share-meta dropup pull-right">
-                                 <ul>
-                                     <li class="dropup">
-                                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-share-alt"></i></a>
-                                         <ul class="dropdown-menu">
-                                             <li>
-                                                 <a href="#"><i class="fa fa-facebook"></i></a>
-                                             </li>
-                                             <li>
-                                                 <a href="#"><i class="fa fa-twitter"></i></a>
-                                             </li>
-                                             <li>
-                                                 <a href="#"><i class="fa fa-instagram"></i></a>
-                                             </li>
-                                             <li>
-                                                 <a href="#"><i class="fa fa-google-plus"></i></a>
-                                             </li>
-                                         </ul>
-                                     </li>
-                                     <li>
-                                         <a href="#"><i class="fa fa-star-o"></i></a>
-                                     </li>
-                                 </ul>
-                             </div>
-                          </div><!-- /.bottom-content -->
-                      </div><!-- /.text-content -->
-                  </div><!-- /.partments-content -->
-              </div><!-- /.col-md-4 -->
-              <div class="clearfix hidden-md hidden-lg"></div>
-              <div class="col-md-4 col-sm-6 col-xs-6">
-                  <div class="apartments-content">
-                      <div class="image-content">
-                          <a href="apartment-single.html"><img src="{{asset('frontend/assets/images/apartment/apartment-three.png')}}" alt="apartment" /></a>
-                      </div><!-- /.image-content -->
-                  
-                      <div class="text-content">
-                          <div class="top-content">
-                              <h3><a href="apartment-single.html">Family Apartment</a></h3>
-                              <span>
-                                  <i class="fa fa-map-marker"></i>
-                                  Dhanmondi, Dhaka
-                              </span> 
-                          </div><!-- /.top-content -->
-                          <div class="bottom-content clearfix">
-                              <div class="meta-bed-room">
-                                  <i class="fa fa-bed"></i> 3 Bedrooms
-                              </div>
-                              <div class="meta-bath-room">                                
-                                  <i class="fa fa-bath"></i>2 Bathroom
-                              </div>
-                              <span class="clearfix"></span>
-                              <div class="rent-price pull-left">
-                                  $200
-                              </div>
-                              <div class="share-meta dropup pull-right">
-                                  <ul>
-                                      <li class="dropup">
-                                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-share-alt"></i></a>
-                                          <ul class="dropdown-menu">
-                                              <li>
-                                                  <a href="#"><i class="fa fa-facebook"></i></a>
-                                              </li>
-                                              <li>
-                                                  <a href="#"><i class="fa fa-twitter"></i></a>
-                                              </li>
-                                              <li>
-                                                  <a href="#"><i class="fa fa-instagram"></i></a>
-                                              </li>
-                                              <li>
-                                                  <a href="#"><i class="fa fa-google-plus"></i></a>
-                                              </li>
-                                          </ul>
-                                      </li>
-                                      <li>
-                                          <a href="#"><i class="fa fa-star-o"></i></a>
-                                      </li>
-                                  </ul>
-                              </div>
-                          </div><!-- /.bottom-content -->
-                      </div><!-- /.text-content -->
-                  </div><!-- /.partments-content -->
-              </div><!-- /.col-md-4 -->
-              <div class="clearfix hidden-xs hidden-sm"></div>
-              <div class="col-md-4 col-sm-6 col-xs-6">
-                  <div class="apartments-content">
-                      <div class="image-content">
-                          <a href="apartment-single.html"><img src="{{asset('frontend/assets/images/apartment/apartment-four.png')}}" alt="apartment" /></a>
-                      </div><!-- /.image-content -->
-                  
-                      <div class="text-content">
-                          <div class="top-content">
-                              <h3><a href="apartment-single.html">Family Apartment</a></h3>
-                              <span>
-                                  <i class="fa fa-map-marker"></i>
-                                  Dhanmondi, Dhaka
-                              </span> 
-                          </div><!-- /.top-content -->
-                          <div class="bottom-content clearfix">
-                              <div class="meta-bed-room">
-                                  <i class="fa fa-bed"></i> 3 Bedrooms
-                              </div>
-                              <div class="meta-bath-room">                                
-                                  <i class="fa fa-bath"></i>2 Bathroom
-                              </div>
-                              <span class="clearfix"></span>
-                              <div class="rent-price pull-left">
-                                  $200
-                              </div>
-                              <div class="share-meta dropup pull-right">
-                                  <ul>
-                                      <li class="dropup">
-                                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-share-alt"></i></a>
-                                          <ul class="dropdown-menu">
-                                              <li>
-                                                  <a href="#"><i class="fa fa-facebook"></i></a>
-                                              </li>
-                                              <li>
-                                                  <a href="#"><i class="fa fa-twitter"></i></a>
-                                              </li>
-                                              <li>
-                                                  <a href="#"><i class="fa fa-instagram"></i></a>
-                                              </li>
-                                              <li>
-                                                  <a href="#"><i class="fa fa-google-plus"></i></a>
-                                              </li>
-                                          </ul>
-                                      </li>
-                                      <li>
-                                          <a href="#"><i class="fa fa-star-o"></i></a>
-                                      </li>
-                                  </ul>
-                              </div>
-                          </div><!-- /.bottom-content -->
-                      </div><!-- /.text-content -->
-                  </div><!-- /.partments-content -->
-              </div><!-- /.col-md-4 -->
-              <div class="clearfix hidden-md hidden-lg"></div>
-              <div class="col-md-4 col-sm-6 col-xs-6">
-                  <div class="apartments-content">
-                      <div class="image-content">
-                          <a href="apartment-single.html"><img src="{{asset('frontend/assets/images/apartment/apartment-five.png')}}" alt="apartment" /></a>
-                      </div><!-- /.image-content -->
-                  
-                      <div class="text-content">
-                          <div class="top-content">
-                              <h3><a href="apartment-single.html">Family Apartment</a></h3>
-                              <span>
-                                  <i class="fa fa-map-marker"></i>
-                                  Dhanmondi, Dhaka
-                              </span> 
-                          </div><!-- /.top-content -->
-                          <div class="bottom-content clearfix">
-                              <div class="meta-bed-room">
-                                  <i class="fa fa-bed"></i> 3 Bedrooms
-                              </div>
-                              <div class="meta-bath-room">                                
-                                  <i class="fa fa-bath"></i>2 Bathroom
-                              </div>
-                              <span class="clearfix"></span>
-                              <div class="rent-price pull-left">
-                                  $200
-                              </div>
-                              <div class="share-meta dropup pull-right">
-                                  <ul>
-                                      <li class="dropup">
-                                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-share-alt"></i></a>
-                                          <ul class="dropdown-menu">
-                                              <li>
-                                                  <a href="#"><i class="fa fa-facebook"></i></a>
-                                              </li>
-                                              <li>
-                                                  <a href="#"><i class="fa fa-twitter"></i></a>
-                                              </li>
-                                              <li>
-                                                  <a href="#"><i class="fa fa-instagram"></i></a>
-                                              </li>
-                                              <li>
-                                                  <a href="#"><i class="fa fa-google-plus"></i></a>
-                                              </li>
-                                          </ul>
-                                      </li>
-                                      <li>
-                                          <a href="#"><i class="fa fa-star-o"></i></a>
-                                      </li>
-                                  </ul>
-                              </div>
-                          </div><!-- /.bottom-content -->
-                      </div><!-- /.text-content -->
-                  </div><!-- /.partments-content -->
-              </div><!-- /.col-md-4 -->
-              <div class="col-md-4 col-sm-6 col-xs-6">
-                  <div class="apartments-content">
-                      <div class="image-content">
-                          <a href="apartment-single.html"><img src="{{asset('frontend/assets/images/apartment/apartment-six.png')}}" alt="apartment" /></a>
-                      </div><!-- /.image-content -->
-                  
-                      <div class="text-content">
-                          <div class="top-content">
-                              <h3><a href="apartment-single.html">Family Apartment</a></h3>
-                              <span>
-                                  <i class="fa fa-map-marker"></i>
-                                  Dhanmondi, Dhaka
-                              </span> 
-                          </div><!-- /.top-content -->
-                          <div class="bottom-content clearfix">
-                              <div class="meta-bed-room">
-                                  <i class="fa fa-bed"></i> 3 Bedrooms
-                              </div>
-                              <div class="meta-bath-room">                                
-                                  <i class="fa fa-bath"></i>2 Bathroom
-                              </div>
-                              <span class="clearfix"></span>
-                              <div class="rent-price pull-left">
-                                  $200
-                              </div>
-                              <div class="share-meta dropup pull-right">
-                                  <ul>
-                                      <li class="dropup">
-                                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-share-alt"></i></a>
-                                          <ul class="dropdown-menu">
-                                              <li>
-                                                  <a href="#"><i class="fa fa-facebook"></i></a>
-                                              </li>
-                                              <li>
-                                                  <a href="#"><i class="fa fa-twitter"></i></a>
-                                              </li>
-                                              <li>
-                                                  <a href="#"><i class="fa fa-instagram"></i></a>
-                                              </li>
-                                              <li>
-                                                  <a href="#"><i class="fa fa-google-plus"></i></a>
-                                              </li>
-                                          </ul>
-                                      </li>
-                                      <li>
-                                          <a href="#"><i class="fa fa-star-o"></i></a>
-                                      </li>
-                                  </ul>
-                              </div>
-                          </div><!-- /.bottom-content -->
-                      </div><!-- /.text-content -->
-                  </div><!-- /.partments-content -->
-              </div><!-- /.col-md-4 -->
+              @endforeach
           </div><!-- /.row -->
           <a href="#" class="button nevy-button">All Apartments</a>
       </div><!-- /.container -->
